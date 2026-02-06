7cdbd0e4b3d570d378d97fd642e9e7255e3eef5a
Deivid Santos <deividsantos1506@gmail.com>
2026-02-06 02:07:36 -0300

    Passagem de dois parametros

--- aula05/src/components/FilhoFunction.tsx
+++ aula05/src/components/FilhoFunction.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 
+interface FilhoFunctionProps {
+  onChildClick: () => void;
+  onChildClick2: () => void;
+}
 
-const FilhoFunction = ({ onChildClick }: { onChildClick: () => void }) => {
-    
+const FilhoFunction = ({ onChildClick, onChildClick2 }: FilhoFunctionProps) => {
   return (
-    <button onClick={onChildClick}>Clique aqui</button>
-  );
-  
+    <div>
+      <button onClick={onChildClick}>Click me</button>
+      <button onClick={onChildClick2}>Click me 2</button>
+    </div>
+  )
 }
 
 export default FilhoFunction
--- aula05/src/components/PaiFunction.tsx
+++ aula05/src/components/PaiFunction.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import FilhoFunction from './FilhoFunction.tsx'
+import FilhoFunction from './FilhoFunction'
 
 const PaiFunction = () => {
 
     const handleChildClick = () => {
         console.log('Child button clicked')
     }
+    const handleChildClick2 = () => {
+        console.log('Child button 2 clicked')
+    }
 
   return (
     <div>
-        <FilhoFunction onChildClick={handleChildClick}/>
+        <FilhoFunction onChildClick={handleChildClick} onChildClick2={handleChildClick2}/>
     </div>
   )
 }
