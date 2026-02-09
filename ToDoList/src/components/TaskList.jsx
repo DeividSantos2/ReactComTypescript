7d0976986b3e3ce77332cd8155f7b7340411757c
Deivid Santos <deividsantos1506@gmail.com>
2026-02-08 23:03:39 -0300

    3131

--- ToDoList/src/components/TaskList.jsx
+++ ToDoList/src/components/TaskList.jsx
@@ -1,12 +1,11 @@
-
 import Task from './Task'
 
 const TaskList = ({tasks}) => {
   return (
     <ul>
-       <Task/>
        {tasks.map((task) => (
         <Task key={task.id} title={task.title} completed={task.completed}/>
+       
        ))}
     </ul>
   )
