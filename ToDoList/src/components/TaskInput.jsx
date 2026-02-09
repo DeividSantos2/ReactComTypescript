9f8a0f807e464c23ab5b1a2dcc6bc1a587241c62
Deivid Santos <deividsantos1506@gmail.com>
2026-02-08 22:49:15 -0300

    Styles Added

---
+++ ToDoList/src/components/TaskInput.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TaskInput = () => {
+  return (
+    <>
+    <div>TaskInput</div>
+    <form action="">
+        <input type="text" placeholder='Adicione uma nova tarefa'></input>
+        <button type='submit'>Adicionar Nova Task</button>
+    </form>
+    </>
+  )
+}
+
+export default TaskInput
---
+++ ToDoList/src/components/TaskList.jsx
@@ -0,0 +1,12 @@
+
+import Task from './Task'
+
+const TaskList = () => {
+  return (
+    <ul>
+       <Task/>
+    </ul>
+  )
+}
+
+export default TaskList
